8d52d10a5c88b1ae3674a792ba27827481593971
FatimaH2912 <fashroon111@gmail.com>
2026-07-22 18:53:40 +0300

    Stage 2 Completed

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = 3000;
 
+const tasks=[
+    { id: 1, title: 'Complete Assignment', completed: false },
+    { id: 2, title: 'Buy Plush Toy', completed: true },
+    { id: 3, title: 'Watch Movie', completed: false }
+];
+
 app.get('/', (req,res)=> {
     res.json({
         name : 'Assignment 2',
@@ -15,6 +21,17 @@ app.get('/health',(freq,res)=>{
     res.json({
         status : 'OK',
     });
+});
+
+app.get('/tasks/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const task= tasks.find(t=> t.id === id);
+
+    if(!task){
+        return res.status(404).json({ error: `Task ${id} not found` });
+    }
+
+    res.json(task);
 });
 
 app.listen(PORT, () => {
