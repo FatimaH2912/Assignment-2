058b8987c24337cc900d3495f32f11a1ecaee834
FatimaH2912 <fashroon111@gmail.com>
2026-07-22 23:22:53 +0300

    Stage 4 Completed

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.use(express.json());
 const PORT = 3000;
 
 const tasks=[
-    { id: 1, title: 'Complete Assignment', completed: false },
-    { id: 2, title: 'Buy Plush Toy', completed: true },
-    { id: 3, title: 'Watch Movie', completed: false }
+    { id: 1, title: 'Complete Assignment', done: false },
+    { id: 2, title: 'Buy Plush Toy', done: false },
+    { id: 3, title: 'Watch Movie', done: false }
 ];
 
 app.get('/', (req,res)=> {
@@ -43,11 +43,31 @@ app.post('/tasks',(req,res)=>{
     }
 
     const newTask={
-        id: tasks.length +1, title: title, completed: false
+        id: tasks.length +1, title: title, done: false
     };
 
     tasks.push(newTask);
     res.status(201).json(newTask);
+});
+
+app.put('/tasks/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const task = tasks.find(t => t.id === id);
+
+    if(!task){
+        return res.status(404).json({ error: `Task ${id} not found` });
+    }
+
+    const { title, done } = req.body;
+
+    if (!title || title.trim() === '') {
+        return res.status(400).json({ error: 'Title is required' });
+    }
+
+    task.title = title;
+    task.done = done;
+
+    res.json(task);
 });
 
 app.listen(PORT, () => {
